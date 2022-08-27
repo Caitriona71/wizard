4858ac23772d227ea5252ade7c19fea1aac541f3
Caitriona71 <caitrionaboyle@ymail.com>
2022-08-27 15:44:43 +0000

    Add colored pad variables

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const topLeft = document.getquerySelector('.top-left-pad');
+const topRight = document.getquerySelector('.top-right-pad');
+const bottomLeft = document.getquerySelector('.bottom-left-pad');
+const bottomRight = document.getquerySelector('.bottom-right-pad');
