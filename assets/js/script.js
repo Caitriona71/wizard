282f0be5c93b50ab925a2eb83830f62eec2eb834
Caitriona71 <caitrionaboyle@ymail.com>
2022-08-27 16:02:37 +0000

    Add timeout to flash for a second

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,5 +13,12 @@ const sequence = [
 const flash = (pad) => {
     return new Promise((resolve, reject)m=> {
         pad.className += ' active';
-    }
-}
+        setTimeout(() => {
+            pad.className = pad.className.replace(
+                ' active',
+                ''
+            );
+            resolve();
+        }, 1000);
+        });
+    };
