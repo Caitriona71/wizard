bdc7c10359083705b8b451fc696bb2a5c4c82869
Caitriona71 <caitrionaboyle@ymail.com>
2022-08-27 15:48:03 +0000

    Add sequence variable for random flashing of pads

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,10 @@ const topLeft = document.getquerySelector('.top-left-pad');
 const topRight = document.getquerySelector('.top-right-pad');
 const bottomLeft = document.getquerySelector('.bottom-left-pad');
 const bottomRight = document.getquerySelector('.bottom-right-pad');
+
+const sequence = [
+    topLeft,
+    bottomRight,
+    bottomLeft,
+    topRight
+];
